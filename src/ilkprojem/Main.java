32680fe49f2a2fc724f4fa84fb35373bf5f04a71
Musa1406 <91537282+Musa1406@users.noreply.github.com>
2021-09-28 13:58:35 +0300

    Initial commit

---
+++ src/ilkprojem/Main.java
@@ -0,0 +1,15 @@
+package ilkprojem;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// sysout ctrl space
+	System.out.println("Merhaba Java");
+	System.out.print("Musa");
+	System.out.print(" ");
+	System.out.println("Acili");
+	System.out.println("Musa"+" "+"Acili");
+	System.out.println("Yaþ :"+15);		
+	}
+}
